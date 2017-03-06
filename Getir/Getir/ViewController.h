e2cecbcacb66ca410ed24eaf76d0db331d774ef9
Buğra ATAY <bugraatay@me.com>
2017-03-06 23:13:01 +0300

    Request-Response metotları eklendi.

--- Getir/Getir/ViewController.h
+++ Getir/Getir/ViewController.h
@@ -7,8 +7,21 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "AFHTTPSessionManager.h"
+#import "AFURLSessionManager.h"
 
 @interface ViewController : UIViewController
+
+@property (strong, nonatomic) NSMutableDictionary *requestData;
+@property (strong, nonatomic) NSMutableDictionary *responseData;
+
+@property (weak, nonatomic) IBOutlet UITextField *txt_Mail;
+@property (weak, nonatomic) IBOutlet UITextField *txt_AdSoyad;
+@property (weak, nonatomic) IBOutlet UITextField *txt_Telefon;
+
+- (IBAction)btn_Gonder:(id)sender;
+
+- (void)postRequestWithData:(NSMutableDictionary *)requestData getData:(NSMutableDictionary *)responseData;
 
 
 @end
--- Getir/Getir/ViewController.m
+++ Getir/Getir/ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    _requestData = [NSMutableDictionary new];
+    _responseData = [NSMutableDictionary new];
 }
 
 
@@ -25,5 +27,51 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+- (IBAction)btn_Gonder:(id)sender {
+    _requestData = [NSMutableDictionary dictionaryWithObjectsAndKeys:
+             _txt_Mail.text, @"email",
+             _txt_AdSoyad.text, @"name",
+             _txt_Telefon.text, @"gsm",
+             nil];
+    [self postRequestWithData:_requestData getData:_responseData];
+}
+
+-(void)postRequestWithData:(NSMutableDictionary *)requestData getData:(NSMutableDictionary *)responseData{
+    NSError *error;
+    NSData *jsonData = [NSJSONSerialization
+                        dataWithJSONObject:requestData
+                        options:NSJSONWritingPrettyPrinted
+                        error:&error];
+    
+    NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+    
+    AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
+    
+    NSMutableURLRequest *req = [[AFJSONRequestSerializer serializer] requestWithMethod:@"POST"
+                                                                             URLString:@"https://getir-bitaksi-hackathon.herokuapp.com/getElements"
+                                                                            parameters:nil
+                                                                                 error:nil];
+    
+    req.timeoutInterval= [[[NSUserDefaults standardUserDefaults] valueForKey:@"timeoutInterval"] longValue];
+    [req setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    [req setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+    [req setHTTPBody:[jsonString dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    
+    [[manager dataTaskWithRequest:req completionHandler:^(NSURLResponse * _Nonnull response, id  _Nullable responseObject, NSError * _Nullable error) {
+        
+        if (!error) {
+            _responseData = [NSMutableDictionary dictionaryWithDictionary:responseObject];
+            NSLog(@"response başarılı %@", _responseData);
+//            for(id item in [_responseData objectForKey:@"elements"]){
+//                
+//                NSLog(@"data: %@", item);
+//            }
+        } else
+            NSLog(@"managePerson metodunda bi hata var: %@, %@, %@", error, response, responseObject);
+        
+    }] resume];
+}
 
 @end
